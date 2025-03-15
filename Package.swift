6d7b6a35f6fd2891d9eaa08c7f00382551b8af57
Tim Oliver <git@tim.dev>
2025-03-15 12:43:41 +0900

    Added initial SPM files

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PhotoSorter2",
+    targets: [
+        .executableTarget(
+            name: "PhotoSorter2"),
+    ]
+)
